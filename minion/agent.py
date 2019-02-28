a235330891c971808f912ab8fc76cb8f07d8ac38
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-01 04:32:16 +0530

    Adds the lambda agent

---
+++ minion/agent.py
@@ -0,0 +1,19 @@
+import importlib
+
+def load_function(module_name, function_name):
+    module = importlib.import_module('.'+module_name, 'functions')
+    return getattr(module, function_name)
+
+def lambda_handler(event, context):
+    module_name, _, function_name = event['function'].rpartition('.')
+    args = event['args']
+    kwargs = event['kwargs']
+
+    func = load_function(module_name, function_name)
+
+    return {
+        'result': func(*args, **kwargs)
+    }
+
+def get_lambda_handler():
+    return lambda_handler
