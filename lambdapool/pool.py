a9fcb16afb2301c1c90b6cd39e1165002594d9db
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-02 20:55:30 +0530

    Implements LambdaFunction

--- lambdapool/pool.py
+++ lambdapool/pool.py
@@ -13,21 +13,50 @@ class Context:
         self.lambda_function = lambda_function
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
-        self.region_name = region_name
+        self.region_name = aws_region_name
+
+class LambdaFunction:
+    def __init__(self, context, function_name):
+        self.context = context
+        self.function_name = function_name
+
+    def __call__(self, *args, **kwargs):
+        payload = {
+            'function': self.function_name,
+            'args': args,
+            'kwargs': kwargs
+        }
+
+        return self._invoke_function(payload)
+
+    def _invoke_function(self, payload):
+        logger.info(f"=== Invoking {self.function_name} for {payload}")
+
+        lambda_client = boto3.client(
+            'lambda',
+            aws_access_key_id=self.context.aws_access_key_id,
+            aws_secret_access_key=self.context.aws_secret_access_key,
+            region_name=self.context.region_name,
+        )
+
+
+        response = lambda_client.invoke(
+            FunctionName=self.context.lambda_function,
+            LogType='Tail',
+            Payload=json.dumps(payload).encode('ascii')
+        )
+
+        result = response['Payload'].read().decode('ascii')
+
+        logger.info(f"=== Result for invokation of {self.function_name} on {payload}: {result}")
+
+        return result
 
 class LambdaPool:
-    def __init__(self, workers: int, lambda_function: str, aws_access_key_id: str=None, aws_secret_access_key: str=None, aws_region_name: str=None):
+    def __init__(self, workers: int, lambda_function: str, aws_access_key_id: str=None, aws_secret_access_key: str=None, region_name: str=None):
         self.workers = workers
         self.pool = ThreadPool(self.workers)
-        self.lambda_function = lambda_function
-        self.context = Context(lambda_function, aws_access_key_id, aws_secret_access_key, aws_region_name)
-        self.lambda_client = boto3.client(
-            'lambda',
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            region_name=region_name,
-            endpoint_url=endpoint_url
-        )
+        self.context = Context(lambda_function, aws_access_key_id, aws_secret_access_key, region_name)
 
     def map(self, function: str, parameters: List[dict]):
         f = functools.partial(self.invoke_function, self.lambda_function)
