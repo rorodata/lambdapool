422da990040a5541b7a040caf8da07670a7f16dc
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-06 06:36:55 +0530

    Implements create cli sub command and makes passing in requirements.txt optional

--- lambdapool/__init__.py
+++ lambdapool/__init__.py
@@ -1,3 +1,4 @@
 from .pool import LambdaPool
 from .agent import lambda_handler
 from .cli import cli
+from .function import LambdaFunction
--- lambdapool/cli.py
+++ lambdapool/cli.py
@@ -1,19 +1,21 @@
 import click
 import os
 
+from .function import LambdaFunction
+
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.option('--requirements', '-r', required=True)
+@click.option('--requirements', '-r', type=click.Path(exists=True))
 @click.argument('function_name', nargs=1)
 @click.argument('paths', nargs=-1, type=click.Path(exists=True))
 def create(function_name, paths, requirements):
-    click.echo('Creating lambdapool function...')
-    click.echo(f'Function: {function_name}')
-    click.echo(f'Paths: {paths}')
-    click.echo(f'Requirements: {requirements}')
+    click.echo('=== Creating lambdapool function ===')
+    func = LambdaFunction(function_name, paths, requirements)
+    func.create()
+    click.echo(f'=== Succesfully created lambdapool function {function_name} ===')
 
 @cli.command()
 def list():
