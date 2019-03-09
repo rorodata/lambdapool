938ef83491ad63695906a014b56d7afd5fe2abca
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-09 23:26:04 +0530

    Makes setup.py take the version dynamically

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
+import os
 import pathlib
 from setuptools import setup
+
+def get_version():
+    """Returns the package version taken from version.py.
+    """
+    root = os.path.dirname(__file__)
+    version_path = os.path.join(root, "lambdapool/version.py")
+    with open(version_path) as f:
+        code = f.read()
+        env = {}
+        exec(code, env, env)
+        return env['__version__']
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -10,7 +22,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="lambda-pool",
-    version="0.6",
+    version=get_version(),
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/rorodata/lambda-pool",
