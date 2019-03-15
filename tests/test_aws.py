15a5fa8695cbdfbc782ca40acfe11ec65d84f31b
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-16 00:17:36 +0530

    Adds boilerplate for testing aws functions

---
+++ tests/test_aws.py
@@ -0,0 +1,18 @@
+class TestRole:
+    def test_role_create(self):
+        pass
+
+    def test_role_exists(self):
+        pass
+
+    def test_role_policy_attachment(self):
+        pass
+
+    def test_get_role(self):
+        pass
+
+    def test_role_delete(self):
+        pass
+
+    def test_role_get_arn(self):
+        pass
