84c21b27371041ba5bc9246b7a4b398c8c8f1990
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-19 09:57:16 +0530

    Adds preliminary tests of cli

---
+++ tests/test_cli.py
@@ -0,0 +1,22 @@
+from click.testing import CliRunner
+
+from lambdapool.cli import cli
+
+class TestCli:
+    def test_list_no_functions(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, 'list')
+        assert result.exit_code == 0
+        assert len(result.output.split('\n')) == 3
+
+    def test_delete_function_does_not_exist(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ['delete', 'test-function'])
+        assert result.exit_code != 0
+        assert 'test-function does not exist' in result.output
+
+    def test_update_function_does_not_exist(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ['update', 'test-function', 'test.py'])
+        assert result.exit_code != 0
+        assert 'test-function does not exist' in result.output
