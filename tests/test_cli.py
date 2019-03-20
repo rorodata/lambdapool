70e3b27cf5de577e43622bd914cb44322c554571
Nabarun Pal <pal.nabarun95@gmail.com>
2019-03-20 10:09:02 +0530

    Adds comprehensive tests for create subcommand

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -30,15 +30,79 @@ class TestCli:
         with open('echo.py', 'w') as f:
             f.write(ECHO_CODE)
 
+        with open('requirements.txt', 'w') as f:
+            f.write('requests\n')
+
     @pytest.fixture
     def function(self, function_code):
         result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py'])
         assert result.exit_code == 0
 
-    def test_create_function(self, function_code):
+    def test_create_function_success(self, function_code):
         result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py'])
         assert result.exit_code == 0
         assert 'Succesfully created lambdapool function test-function' in result.output
+
+    def test_create_function_exists_error(self, function):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py'])
+        assert result.exit_code != 0
+        print(result.output)
+        assert 'lambdapool function test-function already exists' in result.output
+
+    def test_create_function_with_requirements(self, function_code):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--requirements', 'requirements.txt'])
+        assert result.exit_code == 0
+        assert 'Succesfully created lambdapool function test-function' in result.output
+
+    def test_create_function_with_memory_success(self, function_code):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--memory', '192'])
+        assert result.exit_code == 0
+        assert 'Succesfully created lambdapool function test-function' in result.output
+
+        result = self.runner.invoke(cli, ['list'])
+        assert result.exit_code == 0
+        assert '192' in result.output
+
+    def test_create_function_with_memory_failed(self, function_code):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--memory', '0'])
+        assert result.exit_code != 0
+        assert 'Invalid memory size provided' in result.output
+
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--memory', '163'])
+        assert result.exit_code != 0
+        assert 'Invalid memory size provided' in result.output
+
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--memory', '4096'])
+        assert result.exit_code != 0
+        assert 'Invalid memory size provided' in result.output
+
+    def test_create_function_with_timeout_success(self, function_code):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--timeout', '300'])
+        assert result.exit_code == 0
+        assert 'Succesfully created lambdapool function test-function' in result.output
+
+        result = self.runner.invoke(cli, ['list'])
+        assert result.exit_code == 0
+        assert '300' in result.output
+
+    def test_create_function_with_timeout_failed(self, function_code):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--timeout', '0'])
+        assert result.exit_code != 0
+        assert 'Invalid timeout provided' in result.output
+
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--timeout', '901'])
+        assert result.exit_code != 0
+        assert 'Invalid timeout provided' in result.output
+
+    def test_create_function_with_all_parameters(self, function_code):
+        result = self.runner.invoke(cli, ['create', 'test-function', 'echo.py', '--requirements', 'requirements.txt', '--memory', '192', '--timeout', '300'])
+        assert result.exit_code == 0
+        assert 'Succesfully created lambdapool function test-function' in result.output
+
+        result = self.runner.invoke(cli, ['list'])
+        assert result.exit_code == 0
+        assert '192' in result.output
+        assert '300' in result.output
 
     def test_list_no_functions(self):
         result = self.runner.invoke(cli, 'list')
